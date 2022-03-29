ae197b19da9f7208423bcddb3772e07c2675fd59
Will McLean <willmcl@gmail.com>
2022-03-29 14:35:34 +0100

    Build the header.

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,23 +1,33 @@
-import { Link } from "gatsby";
-import React, { Component } from "react";
-import Container from "../atoms/Container";
+import React from "react";
 import styled from "styled-components";
+import logo from "../../assets/img/bear.png";
 
 const Holder = styled.header`
+  padding: 24px 24px 0 24px;
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: 1fr 3fr;
+  align-items: center;
+  @media( ${props => props.theme.breakpoints.md} ) {
+    grid-template-columns: 1fr 1fr;
+  }
+  img {
+    width: 75px;
+    height: auto;
+    display: block;
+  }
+  p {
+    text-transform: uppercase;
+  }
 `;
 
-class Header extends Component {
-  render() {
+function Header() {
     return (
-      <Container>
-        <Holder>
-          <p>
-            <Link to="/">Logo</Link>
-          </p>
-        </Holder>
-      </Container>
+      <Holder>
+        <img src={logo} alt="Bear meets eagle on fire"/>
+        <p>We help good brands and people think<br/> and make things differently.</p>
+      </Holder>
     );
-  }
 }
 
 export default Header;
