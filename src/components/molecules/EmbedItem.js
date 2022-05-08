71373f1ec37f0eeb8ff4c31c2cf5c067ccda7a0a
Will McLean <willmcl@gmail.com>
2022-05-08 09:24:43 +0100

    Refactor the close embed overlay button.

--- src/components/molecules/EmbedItem.js
+++ src/components/molecules/EmbedItem.js
@@ -54,26 +54,38 @@ const OverlayHolder = styled.div`
   color: #333;
   background-color: #000;
   transform-origin: bottom left;
+  font-family: 'Gotham', "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   
   .close {
     position: absolute;
-    top: ${(48 - 15) / 2}px;
-    left: 15px;
-    margin: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     text-transform: uppercase;
-    @media( ${props => props.theme.breakpoints.md} ) {
-      left: 24px;
+    
+    .exit-text, .cross {
+      margin: 0;
+      position: absolute;
+      top: ${(48 - 15) / 2}px;
+      left: 15px;
+      @media( ${props => props.theme.breakpoints.md} ) {
+        left: 24px;
+      }
+    }
+    .cross {
+      left: auto;
+      right: 15px;
+      @media( ${props => props.theme.breakpoints.md} ) {
+        left: auto;
+        right: 24px;
+      }
     }
   }
 
   iframe,
   object,
   embed {
-    //position: absolute;
-    //top: 48px;
-    //left: 0;
-    //width: 100%;
-    //height: calc(100% - 96px);
     position: absolute;
     top: 50%;
     left: 50%;
@@ -173,8 +185,11 @@ function EmbedItem({embed, canPlay, poster}) {
         classNames="overlay-holder"
       >
         <OverlayHolder>
+          <button className="close" onClick={() => setOverlayOpen(false)}>
+            <p className="exit-text">Exit full screen</p>
+            <p className="cross">X</p>
+          </button>
           <div dangerouslySetInnerHTML={{__html: newHtml}} />
-          <button className="close" onClick={() => setOverlayOpen(false)}>Exit full screen</button>
         </OverlayHolder>
       </CSSTransition>
 
