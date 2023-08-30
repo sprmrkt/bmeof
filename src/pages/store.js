a4eea0e300cad1cc439733cd527502d6f9ee7c89
Johnny Crisp <johnnycrisp2@gmail.com>
2023-08-30 12:50:24 +0100

    update url

--- src/pages/store.js
+++ src/pages/store.js
@@ -82,12 +82,12 @@ const store = () => {
             className="snipcart-add-item"
             data-item-id="product-2"
             data-item-price={29.99}
-            data-item-url="/store"
+            data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
+            data-item-url="/"
+            data-item-image={bear}
             data-item-name="Product 2"
-            data-item-custom1-name="Size"
-            data-item-custom1-options="6|6.5|7|7.5|8|8.5|9"
-            data-item-custom2-name="This is a gift"
-            data-item-custom2-type="checkbox"
+            data-item-custom2-name="Custom"
+            data-item-custom2-options="Custom 1|Custom 2|Custom 3[+50.00]"
           >
             Add to cart
           </button>
@@ -107,7 +107,7 @@ const store = () => {
             className="snipcart-add-item"
             data-item-id="product-3"
             data-item-price={19.99}
-            data-item-url="/store"
+            data-item-url="/"
             data-item-name="Product 3"
             data-item-custom1-name="Size"
             data-item-custom1-options="6|6.5|7|7.5|8|8.5|9"
