a63a770d326d1163ebcdf62780ae850381f0e8c5
Johnny Crisp <johnnycrisp2@gmail.com>
2023-08-31 12:46:29 +0100

    update stickers in useEffect

--- src/components/atoms/Stickers.js
+++ src/components/atoms/Stickers.js
@@ -1,8 +1,9 @@
 import { GatsbyImage } from "gatsby-plugin-image";
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import Draggable from "react-draggable"; // The default
 import { useWindowSize } from "react-use";
+import { useState } from "react";
 
 const Holder = styled.div`
   position: absolute;
@@ -30,36 +31,40 @@ const Sticker = styled.div`
     width: 6rem;
   }
 `;
-
 function Stickers({ data }) {
   const { height, width } = useWindowSize();
+  const [stickers, setStickers] = useState([]);
 
-  const getStickers = (data) => {
-    return data.stickers.nodes.map((sticker, i) => {
-      // Generate a random yPos and xPos for each sticker
-      const randomYPos = Math.floor(Math.random() * (height - 100 + 1));
-      const randomXPos = Math.floor(Math.random() * (width - 50 + 1));
+  useEffect(() => {
+    const generateStickers = () => {
+      const newStickers = data.stickers.nodes.map((sticker, i) => {
+        // Generate a random yPos and xPos for each sticker
+        const randomYPos = Math.floor(Math.random() * (height - 200 + 1));
+        const randomXPos = Math.floor(Math.random() * (width - 100 + 1));
 
-      return (
-        <Draggable
-          positionOffset={{
-            x: randomXPos,
-            y: randomYPos,
-          }}
-          key={i}
-        >
-          <Sticker>
-            <GatsbyImage
-              image={sticker.data.image.gatsbyImageData}
-              alt={sticker.data.image.alt || "sticker"}
-            />
-          </Sticker>
-        </Draggable>
-      );
-    });
-  };
+        return (
+          <Draggable
+            positionOffset={{
+              x: randomXPos,
+              y: randomYPos,
+            }}
+            key={i}
+          >
+            <Sticker>
+              <GatsbyImage
+                image={sticker.data.image.gatsbyImageData}
+                alt={sticker.data.image.alt || "sticker"}
+              />
+            </Sticker>
+          </Draggable>
+        );
+      });
 
-  const stickers = getStickers(data);
+      setStickers(newStickers);
+    };
+
+    generateStickers();
+  }, [data.stickers.nodes, height, width]);
 
   return <Holder>{stickers}</Holder>;
 }
