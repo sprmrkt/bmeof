5d3420b5c5445e86a6a5e55856813c4294f9bb0b
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-09-27 16:04:13 +1000

    worknav background

--- src/components/atoms/CloseButton.js
+++ src/components/atoms/CloseButton.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-import {useStore} from "../../utils/store";
-import {manualKerning} from "../../utils/helpers";
+import { useStore } from "../../utils/store";
+import { manualKerning } from "../../utils/helpers";
 
 const Holder = styled.div`
   width: 100%;
   overflow: hidden;
   padding-top: 48px;
-  @media (${props => props.theme.breakpoints.md}) {
+  background-color: ${({ theme }) => theme.colors.white};
+
+  @media (${(props) => props.theme.breakpoints.md}) {
     padding: 0 0 0 12px;
   }
 
@@ -23,12 +25,12 @@ const Holder = styled.div`
     padding: 12px 0;
     transition: color 0.5s linear;
 
-    @media (${props => props.theme.breakpoints.md}) {
+    @media (${(props) => props.theme.breakpoints.md}) {
       padding: 12px 0 24px 0;
     }
 
     &:hover {
-      @media (${props => props.theme.breakpoints.md}) {
+      @media (${(props) => props.theme.breakpoints.md}) {
         color: rgb(70, 70, 70);
       }
     }
@@ -41,27 +43,27 @@ const Copyright = styled.div`
   grid-template-columns: 1fr 3fr;
   align-items: end;
   padding: 0 15px calc(15px + 48px);
-  @media (${props => props.theme.breakpoints.md}) {
+  @media (${(props) => props.theme.breakpoints.md}) {
     grid-template-columns: 1fr 1fr;
     padding: 0 24px;
   }
   p {
     margin-bottom: 12px;
     margin-top: 0;
-    @media (${props => props.theme.breakpoints.md}) {
+    @media (${(props) => props.theme.breakpoints.md}) {
       margin-bottom: 24px;
       margin-top: 0;
     }
   }
 `;
 
-function CloseButton({border}) {
-  const holderClasses = classNames("close-button", {"has-border": border});
+function CloseButton({ border }) {
+  const holderClasses = classNames("close-button", { "has-border": border });
   const { closeNav } = useStore();
 
   const clickHandler = () => {
     closeNav();
-  }
+  };
 
   return (
     <>
