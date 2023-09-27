75f3aa56320ee761602c45b80518de888853d805
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-09-27 14:39:33 +1000

    useInitialGlobalNav hook calculating distance

--- src/hooks/useInitialGlobalNavSplit.js
+++ src/hooks/useInitialGlobalNavSplit.js
@@ -1,7 +1,12 @@
-import {useStore} from "../utils/store";
-import {useEffect} from "react";
+import { useStore } from "../utils/store";
+import { useEffect } from "react";
 
-export default function useInitialGlobalNavSplit(navRef, id, index, individualWorkPage = false) {
+export default function useInitialGlobalNavSplit(
+  navRef,
+  id,
+  index,
+  individualWorkPage = false
+) {
   const {
     navSplitIndex,
     setNavSplitIndex,
@@ -11,33 +16,52 @@ export default function useInitialGlobalNavSplit(navRef, id, index, individualWo
     setGlobalNavSplitHappenedOnce,
   } = useStore();
 
-
   useEffect(() => {
-    // 1. Check if we have landed directly on the work page
+    // 1. Check if we have landed directly on the work, studio, hello, or store page
     const el = navRef?.current;
-    if (el && navSplitIndex === null && !globalNavSplitHappenedOnce) {
-      // 1. Scroll global nav to work link
-      const {top, height} = el.querySelector(`#${id}`).getBoundingClientRect();
-      el.scrollTo(0, top + el.scrollTop);
-      // 2. Wait until the scroll (above) has definitely happened then...
-      const split = setTimeout(() => {
-        // 3. Calculate translate distance for nav up and down
-        const windowHeight = window?.innerHeight;
-        let up = -(height - 48);
-        if (individualWorkPage) up -= 48;
-        const down = windowHeight - 48;
-        // 4. Set nav split index to work link
-        // 5. Set nav up and down position
-        setNavSplitIndex(index);
-        setNavUpPosition(up);
-        setNavDownPosition(down);
-        // 6. Set happenedOnce to true
-        setGlobalNavSplitHappenedOnce(true);
-      }, 500);
-      // 7. Clear timeout
-      return () => clearTimeout(split);
-    }
-  }, [index, navSplitIndex, globalNavSplitHappenedOnce, navRef, setNavSplitIndex, setNavUpPosition, setNavDownPosition, setGlobalNavSplitHappenedOnce, id, individualWorkPage]);
+    if (!el || navSplitIndex !== null || globalNavSplitHappenedOnce) return;
+
+    // 2. Scroll global nav to work link
+    const workLink = el.querySelector(`#${id}`);
+
+    const { top } = workLink.getBoundingClientRect();
+    el.scrollTo(0, top + el.scrollTop);
+
+    // 3. Wait until the scroll (above) has definitely happened then...
+    const split = setTimeout(() => {
+      // 4. Calculate translate distance for nav up and down
+      const { top: newTop, bottom, height } = workLink.getBoundingClientRect();
+      const windowHeight = window?.innerHeight;
+      let up = -(bottom - 48);
+      if (individualWorkPage) up -= 48;
+      const down = windowHeight - newTop - height;
+
+      // 5. Set nav split index to work link
+      setNavSplitIndex(index);
+
+      // 6. Set nav up and down position
+      setNavUpPosition(up);
+      setNavDownPosition(down);
+
+      // 7. Set happenedOnce to true
+      setGlobalNavSplitHappenedOnce(true);
+    }, 0);
+
+    // 8. Clear timeout
+    return () => clearTimeout(split);
+  }, [
+    index,
+    navSplitIndex,
+    globalNavSplitHappenedOnce,
+    navRef,
+    setNavSplitIndex,
+    setNavUpPosition,
+    setNavDownPosition,
+    setGlobalNavSplitHappenedOnce,
+    id,
+    individualWorkPage,
+  ]);
 
   return null;
-};
+}
+
