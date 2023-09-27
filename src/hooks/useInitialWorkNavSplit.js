874cda7477bd0a2e89a47b0ff4bcdd26a1cae85c
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-09-27 16:04:00 +1000

    uIWNS hook functionality tweaks

--- src/hooks/useInitialWorkNavSplit.js
+++ src/hooks/useInitialWorkNavSplit.js
@@ -1,5 +1,5 @@
-import {useStore} from "../utils/store";
-import {useEffect} from "react";
+import { useStore } from "../utils/store";
+import { useEffect } from "react";
 
 export default function useInitialWorkNavSplit(workNavRef, id, index) {
   const {
@@ -11,32 +11,50 @@ export default function useInitialWorkNavSplit(workNavRef, id, index) {
     setWorkNavSplitHappenedOnce,
   } = useStore();
 
-
   useEffect(() => {
     // 1. Check if we have landed directly on the work page
     const el = workNavRef?.current;
-    if (el && workNavSplitIndex === null && !workNavSplitHappenedOnce) {
-      // 1. Scroll global nav to work link
-      const {top, height} = el.querySelector(`#work-${id}`).getBoundingClientRect();
-      el.scrollTo(0, top + el.scrollTop);
-      // 2. Wait until the scroll (above) has definitely happened then...
-      const split = setTimeout(() => {
-        // 3. Calculate translate distance for nav up and down
-        const windowHeight = window?.innerHeight;
-        let up = -(height - 48);
-        const down = windowHeight - 48;
-        // 4. Set nav split index to work link
-        // 5. Set nav up and down position
-        setWorkNavSplitIndex(index);
-        setWorkNavUpPosition(up);
-        setWorkNavDownPosition(down);
-        // 6. Set happenedOnce to true
-        setWorkNavSplitHappenedOnce(true);
-      }, 500);
-      // 7. Clear timeout
-      return () => clearTimeout(split);
-    }
-  }, [index, workNavSplitIndex, workNavSplitHappenedOnce, workNavRef, setWorkNavSplitIndex, setWorkNavUpPosition, setWorkNavDownPosition, setWorkNavSplitHappenedOnce, id]);
+    if (!el || workNavSplitIndex !== null || workNavSplitHappenedOnce) return;
+
+    // 2. Scroll global nav to work link
+    const workLink = el.querySelector(`#work-${id}`);
+
+    const { top } = workLink.getBoundingClientRect();
+    el.scrollTo(0, top + el.scrollTop);
+
+    // 3. Wait until the scroll (above) has definitely happened then...
+    const split = setTimeout(() => {
+      // 4. Calculate translate distance for nav up and down
+      const { top: newTop, bottom, height } = workLink.getBoundingClientRect();
+      const windowHeight = window?.innerHeight;
+      let up = -(bottom - 48);
+      const down = windowHeight - newTop - height;
+
+      // 5. Set nav split index to work link
+      setWorkNavSplitIndex(index);
+
+      // 6. Set nav up and down position
+      setWorkNavUpPosition(up);
+      setWorkNavDownPosition(down);
+
+      // 7. Set happenedOnce to true
+      setWorkNavSplitHappenedOnce(true);
+    }, 0);
+
+    // 7. Clear timeout
+    return () => clearTimeout(split);
+  }, [
+    index,
+    workNavSplitIndex,
+    workNavSplitHappenedOnce,
+    workNavRef,
+    setWorkNavSplitIndex,
+    setWorkNavUpPosition,
+    setWorkNavDownPosition,
+    setWorkNavSplitHappenedOnce,
+    id,
+  ]);
 
   return null;
-};
+}
+
