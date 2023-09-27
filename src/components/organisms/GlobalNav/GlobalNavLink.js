f787746712d0b9371cbcc0ec4a0ded9ffc240291
Lane Le Prevost-Smith <lanelps@gmail.com>
2023-09-27 12:47:52 +1000

    calculating down position

--- src/components/organisms/GlobalNav/GlobalNavLink.js
+++ src/components/organisms/GlobalNav/GlobalNavLink.js
@@ -1,31 +1,24 @@
 import React from "react";
-import {navigate} from "gatsby";
+import { navigate } from "gatsby";
 import propTypes from "prop-types";
 
-import {manualKerning} from "../../../utils/helpers";
-import {useStore} from "../../../utils/store";
+import { manualKerning } from "../../../utils/helpers";
+import { useStore } from "../../../utils/store";
 
-function GlobalNavLink({
-  link,
-  index,
-  globalNavRef,
-}) {
+function GlobalNavLink({ link, index, globalNavRef }) {
   //store
-  const { setNavSplitIndex } = useStore();
-  const { setNavUpPosition } = useStore();
-  const { setNavDownPosition } = useStore();
+  const { setNavSplitIndex, setNavUpPosition, setNavDownPosition } = useStore();
 
   // methods
   const calculateTranslateDistance = () => {
     const el = globalNavRef.current.querySelector(`#${link.id}`);
     if (!el) return;
 
-    const {top, bottom, height} = el?.getBoundingClientRect();
+    const { top, bottom, height } = el?.getBoundingClientRect();
 
     const windowHeight = window?.innerHeight;
-    const elHeight = height / 4;
     const up = -(bottom - 48);
-    const down = windowHeight - top + elHeight;
+    const down = windowHeight - top - height;
 
     setNavUpPosition(up);
     setNavDownPosition(down);
@@ -55,7 +48,7 @@ export default GlobalNavLink;
 
 GlobalNavLink.propTypes = {
   link: propTypes.shape({
-    ref: propTypes.shape({current: propTypes.string}),
+    ref: propTypes.shape({ current: propTypes.string }),
     id: propTypes.string,
     slug: propTypes.string,
     label: propTypes.string,
