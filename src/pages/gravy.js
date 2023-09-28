fb91b107bb309bebfe23a04f98b830c1d63e011f
Johnny Crisp <johnnycrisp2@gmail.com>
2023-09-28 11:43:12 +0100

    Create gravy page, static query for data, add link, update stackedImages conditional to avoid build error

---
+++ src/pages/gravy.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import CloseButton from "../components/atoms/CloseButton";
+import Gravy from "../components/organisms/Gravy";
+import useInitialGlobalNavSplit from "../hooks/useInitialGlobalNavSplit";
+
+const Container = styled.div`
+  margin-top: 48px;
+`;
+
+const GravyPage = (props) => {
+  useInitialGlobalNavSplit(props.globalNav, 'gravy', 1);
+  return (
+    <Container>
+      <Gravy />
+      <CloseButton />
+    </Container>
+  );
+};
+
+export default GravyPage;
+
